6a00d8155e56aa631f98136d1d96bb667909b49e
Gianluca Onisanti <gianluca.onisanti@gmail.com>
2025-04-09 01:09:32 -0300

    Commit Pré-Apresentação

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import {
   Button,
@@ -7,34 +7,369 @@ import {
   Grid2,
   Box,
   Typography,
-} from "@mui/material";
-import Image from "next/image";
-import { useStyles } from "../styles/Styler";
-import { Cake, Timer } from "@mui/icons-material";
-import { useState } from "react";
+  Divider,
+  IconButton,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+} from '@mui/material';
+import { useStyles } from '../styles/Styler';
+import { Add, Check, Close, PlayArrow, Timer } from '@mui/icons-material';
+import React, { useEffect, useState } from 'react';
+import { List, ListItem, ListItemText } from '@mui/material';
+import usuarios from './data/usuarios.json';
+import tarefas from './data/tarefas.json';
+import subtarefas from './data/subtarefas.json';
+
+// Definindo tipos específicos para tarefa e subtarefa
+interface Tarefa {
+  id: number;
+  titulo: string;
+  usuario: number;
+  criado_em: string;
+  editado_em: string;
+  entrega_em: string;
+}
+
+interface Subtarefa extends Tarefa {
+  tarefa_pai: number;
+}
 
 export default function HomePage() {
   const theme = useTheme();
   const sx = useStyles();
+  const [data, setData] = useState<{
+    usuarios: any[];
+    tarefas: Tarefa[];
+    subtarefas: Subtarefa[];
+  }>({
+    usuarios,
+    tarefas,
+    subtarefas,
+  });
+
+  const [newData, setNewData] = useState({ titulo: '', usuario: -1 });
+
+  const [adder, setAdder] = useState({ status: false, type: '', index: -1 });
+
+  const handleClick = (e: any) => {
+    const newData = {
+      ...data,
+      usuarios: data.usuarios.map((usuario, index) => {
+        if (index === 0) {
+          return { ...usuario, id: 20 };
+        }
+        return usuario;
+      }),
+    };
+    setData(newData);
+  };
+
+  const handleAdding = (e: any) => {
+    console.log(e.target);
+
+    const name = e.target.dataset.name as 'tarefa' | 'subtarefa';
+    const index = Number(e.target.dataset.index) || -1;
+
+    const type = {
+      tarefa: 't',
+      subtarefa: 's',
+    } as const;
+
+    setAdder({ status: true, type: type[name], index: index });
+  };
+
+  function displayArrow(
+    item: any,
+    variant: any,
+    style?: any,
+    bold: boolean = false
+  ) {
+    return (
+      <Typography
+        fontStyle={style}
+        variant={variant}
+        fontWeight={bold ? 'bold' : ''}
+      >
+        {item}
+      </Typography>
+    );
+  }
+
+  function getUser(id: number) {
+    return data.usuarios.filter((item: any) => item.id === id)[0].nome;
+  }
+
+  function adderStyle() {
+    return {
+      gap: 1,
+      color: theme.palette.background.default,
+      backgroundColor: theme.palette.primary.main,
+      transition: 'all 0.3s ease-in-out',
+      '&:hover': {
+        transform: 'scale(1.1)',
+        color: theme.palette.primary.main,
+        backgroundColor: `${theme.palette.primary.main}50`,
+        border: `2px solid ${theme.palette.primary.main}`,
+      },
+    };
+  }
+
+  function userSelector(id: number, task: number) {
+    const handleSelect = (e: any) => {
+      const result = {
+        ...data,
+        subtarefas: data.subtarefas.map((sub, index) => {
+          if (index === task - 1) {
+            return { ...sub, usuario: Number(e.target.value) };
+          }
+          return sub;
+        }),
+      };
+      setData(result);
+    };
+
+    return (
+      <FormControl fullWidth>
+        <InputLabel id="usuario_selector">Responsável</InputLabel>
+        <Select
+          fullWidth
+          name="usuario"
+          value={id}
+          label="Responsável"
+          onChange={handleSelect}
+        >
+          {data.usuarios.map((item: any) => (
+            <MenuItem value={item.id}>{item.nome}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    );
+  }
+
+  const handleNewData = (e: any) => {
+    const { name, value } = e.target;
+
+    setNewData({
+      ...newData,
+      [name]: name === 'usuario' ? Number(value) : value,
+    });
+  };
+
+  const handleAbort = (e: any) => {
+    setAdder({ status: false, type: '', index: -1 });
+    setNewData({ titulo: '', usuario: -1 });
+  };
+
+  const handleCheck = (e: any) => {
+    let newTask: Tarefa | Subtarefa;
+    const today = new Date().toISOString().slice(0, 10);
+
+    if (adder.index > 0) {
+      newTask = {
+        id: data.subtarefas.length + 1,
+        titulo: newData.titulo,
+        usuario: newData.usuario,
+        criado_em: today,
+        editado_em: today,
+        entrega_em: today,
+        tarefa_pai: Number(adder.index),
+      };
+
+      setData({
+        ...data,
+        subtarefas: [...data.subtarefas, newTask as Subtarefa],
+      });
+    } else {
+      newTask = {
+        id: data.tarefas.length + 1,
+        titulo: newData.titulo,
+        usuario: newData.usuario,
+        criado_em: '2025-03-21',
+        editado_em: '2025-03-21',
+        entrega_em: '2025-03-21',
+      };
+      setData({
+        ...data,
+        tarefas: [...data.tarefas, newTask as Tarefa],
+      });
+    }
+    setAdder({ status: false, type: '', index: -1 });
+    setNewData({ titulo: '', usuario: -1 });
+  };
 
   return (
     <Container>
-      <Grid2 mt={4} container spacing={{ xs: 1, md: 6 }} alignItems={"center"}>
-        {/* Para estudo: 
-          XS = ExtraSmall (Mobile); 
-          MD = Medium (Telas P e Tablets); 
-          LG = Large (PCs e TVs); 
-        */}
-        <Grid2
-          sx={{ textAlign: { xs: "center", md: "start", lg: "start" } }}
-          size={{ xs: 12, md: 6, lg: 6 }}
-        >
-          <Button variant="contained" sx={sx.button.primary}>
-            <Typography>{"Iniciar Cronômetro"}</Typography>
-            <Timer fontSize="large" />
-          </Button>
-        </Grid2>
-      </Grid2>
+      <List>
+        {data.tarefas.map((item: any) => (
+          <React.Fragment key={item.id}>
+            <ListItem>
+              <ListItemText
+                primary={displayArrow(item.titulo, 'h6', '', true)}
+                secondary={displayArrow(
+                  `Criado em: ${item.criado_em}`,
+                  'body2',
+                  'italic',
+                  false
+                )}
+              />
+            </ListItem>
+            <List sx={{ margin: '0px 0px 20px 60px' }}>
+              {data.subtarefas
+                .filter((sub: any) => sub.tarefa_pai === item.id)
+                .map((sub: any) => (
+                  <React.Fragment key={sub.id}>
+                    <ListItem key={sub.id}>
+                      <Grid2
+                        container
+                        spacing={{ xs: 1, md: 2 }}
+                        alignItems={'center'}
+                      >
+                        <Grid2 width="max-content" size={6}>
+                          <Box
+                            sx={{
+                              width: '480px',
+                              padding: '5px 10px',
+                              borderRadius: '15px',
+                              backgroundColor: `${theme.palette.primary.main}75`,
+                            }}
+                          >
+                            <ListItemText
+                              primary={displayArrow(sub.titulo, '', '', true)}
+                              secondary={displayArrow(
+                                `Criado em: ${sub.criado_em}`,
+                                'body2',
+                                'italic',
+                                false
+                              )}
+                            />
+                          </Box>
+                        </Grid2>
+                        <Grid2 width="max-content" size={4}>
+                          <Box width={'235px'}>
+                            {userSelector(sub.usuario, sub.id)}
+                          </Box>
+                        </Grid2>
+                        <Grid2 width="max-content" size={1}>
+                          <IconButton sx={sx.timer}>
+                            <PlayArrow fontSize="small" />
+                          </IconButton>
+                        </Grid2>
+                      </Grid2>
+                    </ListItem>
+                  </React.Fragment>
+                ))}
+              <ListItem>
+                {adder.type !== 's' || adder.index !== item.id ? (
+                  <Button
+                    disabled={adder.status}
+                    sx={adderStyle}
+                    onClick={handleAdding}
+                    data-name="subtarefa"
+                    data-index={item.id}
+                  >
+                    <Add sx={{ color: 'inherit', pointerEvents: 'none' }} />
+                    <ListItemText
+                      sx={{ pointerEvents: 'none' }}
+                      primary={displayArrow(
+                        'Adicionar Subtarefa',
+                        'caption',
+                        '',
+                        true
+                      )}
+                    />
+                  </Button>
+                ) : (
+                  <div style={{ display: 'inline-flex', gap: 20 }}>
+                    <TextField
+                      sx={{ width: '420px' }}
+                      name="titulo"
+                      value={newData.titulo}
+                      label="Nome da Subtarefa"
+                      onChange={handleNewData}
+                    />
+                    <FormControl fullWidth>
+                      <InputLabel id="usuario_selector">Responsável</InputLabel>
+                      <Select
+                        fullWidth
+                        name="usuario"
+                        value={newData.usuario}
+                        label="Responsável"
+                        onChange={handleNewData}
+                      >
+                        {data.usuarios.map((item: any) => (
+                          <MenuItem value={item.id}>{item.nome}</MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                    <Button onClick={handleAbort}>
+                      <Close color="error" />
+                    </Button>
+                    {newData.titulo && newData.usuario && (
+                      <Button data-checker="sub" onClick={handleCheck}>
+                        <Check />
+                      </Button>
+                    )}
+                  </div>
+                )}
+              </ListItem>
+            </List>
+            <Divider sx={{ width: '235px' }} />
+            <Divider sx={{ width: '235px' }} />
+            <Divider sx={{ width: '235px' }} />
+          </React.Fragment>
+        ))}
+        <ListItem>
+          {adder.type !== 't' ? (
+            <Button
+              disabled={adder.status}
+              sx={adderStyle}
+              onClick={handleAdding}
+              data-name="tarefa"
+            >
+              <Add sx={{ color: 'inherit', pointerEvents: 'none' }} />
+              <ListItemText
+                sx={{ pointerEvents: 'none' }}
+                primary={displayArrow('Adicionar Tarefa', 'body1', '', true)}
+              />
+            </Button>
+          ) : (
+            <div style={{ display: 'inline-flex', gap: 20 }}>
+              <TextField
+                sx={{ width: '420px' }}
+                name="titulo"
+                value={newData.titulo}
+                label="Nome da Tarefa"
+                onChange={handleNewData}
+              />
+              <FormControl fullWidth>
+                <InputLabel id="usuario_selector">Responsável</InputLabel>
+                <Select
+                  fullWidth
+                  name="usuario"
+                  value={newData.usuario}
+                  label="Responsável"
+                  onChange={handleNewData}
+                >
+                  {data.usuarios.map((item: any) => (
+                    <MenuItem value={item.id}>{item.nome}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <Button onClick={handleAbort}>
+                <Close color="error" />
+              </Button>
+              {newData.titulo && newData.usuario && (
+                <Button onClick={handleCheck}>
+                  <Check />
+                </Button>
+              )}
+            </div>
+          )}
+        </ListItem>
+      </List>
     </Container>
   );
 }
--- src/app/styles/Styler.ts
+++ src/app/styles/Styler.ts
@@ -1,42 +1,42 @@
-import { useTheme } from "@mui/material";
+import { useTheme } from '@mui/material';
 
-export function useSwitcher(mode: "light" | "dark") {
+export function useSwitcher(mode: 'light' | 'dark') {
   const theme = {
     light: {
       palette: {
-        mode: "light", // TEMA CLARO
+        mode: 'light', // TEMA CLARO
         primary: {
-          main: "#82c055",
+          main: '#82c055',
         },
         secondary: {
-          main: "#4c7132",
+          main: '#4c7132',
         },
         background: {
-          default: "#ffffff",
-          paper: "#e2f2d6",
+          default: '#ffffff',
+          paper: '#e2f2d6',
         },
         text: {
-          primary: "#212121",
-          secondary: "#757575",
+          primary: '#212121',
+          secondary: '#757575',
         },
       },
     },
     dark: {
       palette: {
-        mode: "dark", // TEMA ESCURO
+        mode: 'dark', // TEMA ESCURO
         primary: {
-          main: "#7bf225",
+          main: '#7bf225',
         },
         secondary: {
-          main: "#429c01",
+          main: '#429c01',
         },
         background: {
-          default: "#474747",
-          paper: "#424242",
+          default: '#474747',
+          paper: '#424242',
         },
         text: {
-          primary: "#ffffff",
-          secondary: "#bdbdbd",
+          primary: '#ffffff',
+          secondary: '#bdbdbd',
         },
       },
     },
@@ -50,34 +50,43 @@ export function useStyles() {
 
   return {
     toolbar: {
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "325px",
-      borderRadius: "10px",
-      padding: "0 10px",
-      margin: "10px 10px 10px 10px",
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      width: '325px',
+      borderRadius: '10px',
+      padding: '0 10px',
+      margin: '10px 10px 10px 10px',
       backgroundColor: `${theme.palette.secondary.main}25`,
       color: theme.palette.text.primary,
     },
     button: {
       primary: {
-        fontFamily: "font",
+        fontFamily: 'font',
         width: 250,
-        boxShadow: "4px 4px #00000050",
+        boxShadow: '4px 4px #00000050',
         border: 4,
-        borderRadius: "10px",
+        borderRadius: '10px',
         borderColor: theme.palette.secondary.main,
         color: theme.palette.secondary.main,
         backgroundColor: theme.palette.primary.main,
-        justifyContent: "space-around",
-        transition: "all 0.3s ease-in-out",
-        "&:hover": {
+        justifyContent: 'space-around',
+        transition: 'all 0.3s ease-in-out',
+        '&:hover': {
           color: theme.palette.text.primary,
           backgroundColor: theme.palette.secondary.main,
           borderColor: theme.palette.text.primary,
-          transform: "scale(1.1)",
+          transform: 'scale(1.1)',
         },
+      },
+    },
+    timer: {
+      transition: 'all 0.3s ease-in-out',
+      backgroundColor: 'red',
+      color: 'white',
+      '&:hover': {
+        backgroundColor: 'white',
+        color: 'red',
       },
     },
   };
